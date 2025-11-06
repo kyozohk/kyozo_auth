d5cc08df1d22114015484c65e10582a8ce0393d0
Kyozo Tech <tech@kyozo.com>
2025-11-06 01:54:52 +0000

    ok done that, now clean up the code

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -21,15 +21,3 @@ export const signupSchema = z
 export const forgotPasswordSchema = z.object({
   email: z.string().email({ message: "Invalid email address." }),
 });
-
-export const resetPasswordSchema = z
-  .object({
-    password: z
-      .string()
-      .min(8, { message: "Password must be at least 8 characters long." }),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match.",
-    path: ["confirmPassword"],
-  });
